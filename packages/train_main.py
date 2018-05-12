812d21e5c4d6cfe6934996d704fd5c51ea52e654
wltsapp <39146956+wltsapp@users.noreply.github.com>
2018-05-12 14:24:04 +0530

    Update train_main.py

--- packages/train_main.py
+++ packages/train_main.py
@@ -8,7 +8,7 @@ datadir = './pre_img'
 modeldir = './model/20170511-185253.pb'
 classifier_filename = './class/classifier.pkl'
 print ("Training Start")
-obj=training(datadir,modeldir,classifier_filename)
+obj=classifier.training(datadir,modeldir,classifier_filename)
 get_file=obj.main_train()
 print('Saved classifier model to file "%s"' % get_file)
 sys.exit("All Done")
