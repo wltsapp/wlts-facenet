0a7ffdddd3bf55c0860abcc357604bd282249977
wltsapp <39146956+wltsapp@users.noreply.github.com>
2018-05-14 15:31:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ def upload():
     upfile = request.files["upfile"]
     img_file = "image" + str(random.randint(0, 999999)) + os.path.splitext(upfile.filename)[1]
     img_path = os.path.join("./cache/", img_file)
-    upfile.save(img_path)
-    item = predictor.get_image_item(img_path)
     print(img_path);
+    upfile.save(img_path)
+    item = predictor.get_image_item(img_path)    
     os.remove(img_path)
     return json.dumps(item)
 
